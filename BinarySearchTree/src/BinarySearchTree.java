597e1db48267e28c58048d739444f3e1d9e6582c
Darron Brown <DarronBrown88@outlook.com>
2018-04-21 17:18:07 -0600

    binary tree insertion

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,2 +1,49 @@
-public class BinarySearchTree {
+public class BinarySearchTree<T extends Comparable<T>> implements Tree<T> {
+
+    private Node<T> root;
+
+    public void insert( T data ) {
+        if ( root == null ) {
+            root = new Node<T>(data);
+        } else {
+            insertNode(data, root);
+        }
+    }
+
+    @Override
+    public void transversal() {
+
+    }
+
+    @Override
+    public void insertNode(T newData, Node<T> node) {
+        if ( newData.compareTo(node.getData()) < 0 ) {
+            if ( node.getLeftChild() != null) {
+                insertNode(newData, node.getLeftChild());
+            } else {
+                Node<T> newNode = new Node<T>(newData);
+                node.setLeftChild(newNode);
+            }
+        } else {
+            if ( node.getRightChild() != null) {
+                Node<T> newNode = new Node<T>(newData);
+                node.setRightChild(newNode);
+            }
+        }
+    }
+
+    @Override
+    public void delete(T data) {
+
+    }
+
+    @Override
+    public T getMax() {
+        return null;
+    }
+
+    @Override
+    public T getMin() {
+        return null;
+    }
 }
--- BinarySearchTree/src/Tree.java
+++ BinarySearchTree/src/Tree.java
@@ -1,6 +1,6 @@
 public interface Tree<T> {
     public void transversal();
-    public void insert( T data );
+    public void insertNode( T data, Node<T> node);
     public void delete( T data );
     public T getMax();
     public T getMin();
