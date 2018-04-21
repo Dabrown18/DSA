677fbb64c4fad38814119f29d7360ea96b204861
Darron Brown <DarronBrown88@outlook.com>
2018-04-21 16:46:43 -0600

    implemented node and tree class

---
+++ BinarySearchTree/src/Tree.java
@@ -0,0 +1,9 @@
+public interface Tree<T> {
+    public void transversal();
+    public void insert( T data );
+    public void delete( T data );
+    public T getMax();
+    public T getMin();
+
+
+}
